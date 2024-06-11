9e0e2070436c9a1921be9c83702508e43f958048
KK10024 <rlwns1580@naver.com>
2024-06-11 13:20:06 +0900

    admin page del

--- spring-java_01/src/main/java/com/rubypaper/admin/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rubypaper.admin;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.rubypaper.user.SiteUser;
-
-public interface AdminRepository 
-extends JpaRepository<SiteUser, Integer>{
-}
